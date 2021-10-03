653ebde9e1c24ee65d4fc2fc5fbaa37cecec7674
Juan Pablo <juanpablo.codes@gmail.com>
2021-10-03 06:47:47 -0300

    box for charts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, SimpleGrid, Box, Text } from '@chakra-ui/react';
 import { Header } from '../components/Header';
 import { SideBar } from '../components/SideBar';
 
@@ -9,6 +9,20 @@ export default function Dashboard() {
 
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <SideBar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+          </Box>
+
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );

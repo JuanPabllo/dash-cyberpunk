2ccdd9292959a8ec1ea05480fa5439a5030b6d57
Juan Pablo <juanpablo.codes@gmail.com>
2021-10-03 06:03:15 -0300

    create header: add search and logo

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import { Flex, Text, Input, Icon } from '@chakra-ui/react';
+import { Search2Icon } from '@chakra-ui/icons';
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxW={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        Dash Punk
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxW={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          px="4"
+          mr="4"
+          placeholder="Buscar na plataforma"
+          _placeholder={{
+            color: 'gray.400',
+          }}
+        />
+        <Search2Icon w={6} h={6} />
+      </Flex>
+    </Flex>
+  );
+}
